f8f51e6142131032339bd4500172947052ece75f
samrudh123 <55134993+samrudh123@users.noreply.github.com>
2021-06-04 14:18:25 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Username: <?php echo $_REQUEST["username"]; ?><br>
+Email: <?php echo $_REQUEST["email"]; ?><br>
+Password: <?php echo $_REQUEST["pwd"]; ?><br>
+
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+Username: <?php echo $_REQUEST["username"]; ?><br>
+Email: <?php echo $_REQUEST["email"]; ?><br>
+Password: <?php echo $_REQUEST["password"]; ?><br>
+Confirm_password: <?php echo $_REQUEST["confirm_password"]; ?>
+
+</body>
+</html>
